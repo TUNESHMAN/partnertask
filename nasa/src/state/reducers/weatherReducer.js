0591dce30faccc7e2ad8b0cf58b7e9e4409fa5bb
Babatunde <babatundea15@gmail.com>
2020-06-26 19:25:32 +0100

    i defined the initial state of the reducer

---
+++ nasa/src/state/reducers/weatherReducer.js
@@ -0,0 +1,9 @@
+// Import the types
+import { GET_WEATHER } from "../types/types";
+
+// Set the initial state to an empty object
+const initialState = {
+  isFetching: false,
+  error: "",
+  weather: [],
+};
