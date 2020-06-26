b0b9c1ef375b1038647fcfad66018e6dd160215a
Babatunde <babatundea15@gmail.com>
2020-06-26 19:51:28 +0100

    i created the main component

---
+++ nasa/src/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div>
+      <h2>Hello weather</h2>
+    </div>
+  );
+}
+
+export default Main;
--- nasa/src/state/reducers/weatherReducer.js
+++ nasa/src/state/reducers/weatherReducer.js
@@ -7,3 +7,17 @@ const initialState = {
   error: "",
   weather: [],
 };
+
+// The reducer takes in initial state and the action as arguments
+const weatherReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_WEATHER:
+            
+            break;
+    
+        default:
+            return state;
+    }
+};
+
+export default weatherReducer;
