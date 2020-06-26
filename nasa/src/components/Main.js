f6151dda397fae8a44ce1134a2b3563b5761926e
Babatunde <babatundea15@gmail.com>
2020-06-26 21:31:20 +0100

    i connected the state to the component where it will be used

--- nasa/src/components/Main.js
+++ nasa/src/components/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { connect } from "react-redux";
 
-function Main() {
+function Main(props) {
   return (
     <div>
       <h2>Hello weather</h2>
@@ -8,4 +9,14 @@ function Main() {
   );
 }
 
-export default Main;
+// mapStateToProps function is the connection between our state from redux and the main component
+const mapStateToProps = (state) => {
+  return {
+    // I return an object with each property from the initial state declared in the reducer
+    isFetching: state.isFetching,
+    error: state.error,
+    weather: state.weather,
+  };
+};
+// I wrap the export of the main component inside the connect call
+export default connect(mapStateToProps, {})(Main);
